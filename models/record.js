aefed1e4b8ed51f28b7e07f425f52fbc9172e8c5
sd880428 <sd660428@gmail.com>
2023-01-10 21:21:02 +0800

    新增 mongoose models

---
+++ models/record.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const recordSchema = new Schema({
+  name: {
+    type: String,
+    require: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  amount: {
+    type: Number,
+    require: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    index: true,
+    require: true
+  },
+  categoryId: {
+    type: Schema.Types.ObjectId,
+    ref: 'Category',
+    index: true,
+    require:true
+  }
+})
+
+module.exports = mongoose.model('Record', recordSchema)
