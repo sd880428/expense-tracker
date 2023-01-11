40596519a2ed21a3d95b8c778843708d7d43bca3
sd880428 <sd660428@gmail.com>
2023-01-11 21:45:27 +0800

    新增 分類功能

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
-const category = require('./modules/category')
+const search = require('./modules/search')
 const users = require('./modules/users')
 const { authenticator } = require('../middleware/auth')
 const record = require('./modules/records')
 
-router.use('/record', record)
+router.use('/record', authenticator, record)
+router.use('/search', authenticator, search)
 router.use('/users', users)
-router.use('/category', category)
-router.use('/', home)
+router.use('/', authenticator, home)
 
 module.exports = router
--- routes/modules/search.js
+++ routes/modules/search.js
@@ -1,11 +1,34 @@
+const { query } = require('express')
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
 
 router.get('/', (req, res) => {
+  const sort = req.query.sort
   const userId = req.user._id
-  Record.find({ userId }).lean().sort({ date: 'desc' })
+  let displayRecord = req.query.onlyShow ? { categoryId: req.query.onlyShow } : { userId }
+
+  let sortBy = ''
+  switch (sort) {
+    case "name":
+      sortBy = { name: 'asc' }
+      break
+    case "date":
+      sortBy = { date: 'desc' }
+      break
+    case "amount":
+      sortBy = { amount: 'asc' }
+      break
+    case "category":
+      sortBy = { categoryId: 'asc' }
+      break
+    default:
+      sortBy = { date: 'desc' }
+      break
+  }
+
+  Record.find(displayRecord).lean().sort(sortBy)
     .then(records => {
       let totalAmount = 0
       records.forEach(record => totalAmount += record.amount) //得出使用者紀錄總金額
@@ -26,5 +49,6 @@ router.get('/', (req, res) => {
         })
     })
 })
+
 
 module.exports = router
